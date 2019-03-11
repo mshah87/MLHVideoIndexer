a1e16408154ffb4f69bbec47340af55cd1b2c361
Faizaan Chishtie <faizchishtie@gmail.com>
2019-03-11 00:06:51 -0400

    functional

---
+++ apikey.js
@@ -0,0 +1,13 @@
+const key = '9ffcfa6a294b4cee8be42fead347ab4c';
+const userKey = '223b7c2e-59f1-4dc1-82f7-4a11f2d3d99d';
+
+
+getKeys = function(){
+    return {
+        key, userKey
+    };
+}
+
+module.exports = {
+    getKeys
+};
